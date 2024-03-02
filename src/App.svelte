50b08576639be542d9a510c03807d0dd2a88e7b6
Miloszzz2 <97192271+Miloszzz2@users.noreply.github.com>
2024-03-02 23:26:18 +0100

    created functions for showing only app name instead of all path and mini style changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,103 +1,112 @@
 <script lang="ts">
-  import Calendar from "svelte-radix/Calendar.svelte";
-  import EnvelopeClosed from "svelte-radix/EnvelopeClosed.svelte";
-  import Face from "svelte-radix/Face.svelte";
-  import Gear from "svelte-radix/Gear.svelte";
-  import Person from "svelte-radix/Person.svelte";
-  import Rocket from "svelte-radix/Rocket.svelte";
-  import { ModeWatcher } from "mode-watcher";
-  import * as Command from "$lib/components/ui/command";
-  import { appWindow } from "@tauri-apps/api/window";
-  import { register } from "@tauri-apps/api/globalShortcut";
-  import { invoke } from "@tauri-apps/api/tauri";
-  import { onMount } from "svelte";
-  let open = true;
-  let inputvalue = "";
-  let value = "";
-  // Function to delay execution
-  function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-  function fixBackslashes(arr: any) {
-    let res: String[] = [];
-    arr.map((el: String) => {
-      res.push(el.replace(/\\\\/g, "\\"));
+    import { ModeWatcher } from "mode-watcher";
+    import * as Command from "$lib/components/ui/command";
+    import { appWindow } from "@tauri-apps/api/window";
+    import { register } from "@tauri-apps/api/globalShortcut";
+    import { invoke } from "@tauri-apps/api/tauri";
+    import { onMount } from "svelte";
+    import { Layers } from "svelte-radix";
+    let open = true;
+    let inputvalue = "";
+    let value = "";
+
+    function delay(ms: number) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    function fixBackslashes(arr: any) {
+        let res: string[] = [];
+        arr.map((el: string) => {
+            res.push(el.replace(/\\\\/g, "\\").replace(/^\\+|\\+$/g, ""));
+        });
+        return res;
+    }
+
+    function getAppNameFromPath(app: string) {
+        let lastIndex = app.lastIndexOf("\\");
+
+        return (
+            app
+                .substring(lastIndex + 1, app.length - 4)
+                .charAt(0)
+                .toUpperCase() + app.substring(lastIndex + 2, app.length - 4)
+        );
+    }
+    let programs: string[] = [];
+    appWindow.listen("created", async () => {
+        invoke("getprogrampaths").then(
+            (message) => (programs = fixBackslashes(message))
+        );
+        appWindow.setFocus();
+        await register("Control+Shift+K", async () => {
+            if (open === true) {
+                inputvalue = "";
+                open = !open;
+                await delay(500);
+                appWindow.minimize();
+            } else {
+                await appWindow.unminimize();
+                appWindow.setFocus();
+                await delay(500);
+                open = !open;
+            }
+        });
     });
-    return res;
-  }
-  let programs: String[] = [];
-  appWindow.listen("created", async () => {
-    invoke("getprogrampaths").then(
-      (message) => (programs = fixBackslashes(message))
-    );
-    appWindow.setFocus();
-    await register("Control+Shift+K", async () => {
-      if (open === true) {
+    async function closeMenu() {
         inputvalue = "";
         open = !open;
         await delay(500);
         appWindow.minimize();
-      } else {
-        await appWindow.unminimize();
-        appWindow.setFocus();
-        await delay(500);
-        open = !open;
-      }
-    });
-  });
-  async function closeMenu() {
-    inputvalue = "";
-    open = !open;
-    await delay(500);
-    appWindow.minimize();
-  }
-
-  appWindow.emit("created");
-  onMount(() => {
-    function handleKeydown(e: KeyboardEvent) {
-      if (e.key === "Enter") {
-        invoke("runprogram", { path: value });
-        closeMenu();
-      } else if (e.key === "Delete") {
-        let indextodel = 0;
-        programs.forEach((el, index) => {
-          if (el.toLowerCase() === value.toLowerCase()) indextodel = index;
-        });
-        programs.splice(indextodel, 1);
-
-        programs = programs;
-        console.log(programs.length);
-      }
     }
 
-    document.addEventListener("keydown", handleKeydown);
-    return () => {
-      document.removeEventListener("keydown", handleKeydown);
-    };
-  });
+    appWindow.emit("created");
+    onMount(() => {
+        function handleKeydown(e: KeyboardEvent) {
+            if (e.key === "Enter") {
+                invoke("runprogram", { path: value });
+                closeMenu();
+            } else if (e.key === "Delete") {
+                let indextodel = 0;
+                programs.forEach((el, index) => {
+                    if (el.toLowerCase() === value.toLowerCase())
+                        indextodel = index;
+                });
+                programs.splice(indextodel, 1);
+
+                programs = programs;
+                console.log(programs.length);
+            }
+        }
+
+        document.addEventListener("keydown", handleKeydown);
+        return () => {
+            document.removeEventListener("keydown", handleKeydown);
+        };
+    });
 </script>
 
 <Command.Dialog bind:open bind:value>
-  <Command.Input placeholder="Type a command or search..." />
-  <Command.List>
-    <Command.Empty>No results found.</Command.Empty>
-    <Command.Group heading="Apps">
-      {#each programs as app}
-        <Command.Item>
-          <!-- svelte-ignore a11y-no-static-element-interactions -->
-          <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <div
-            on:click={() => {
-              invoke("runprogram", { path: app });
-              closeMenu();
-            }}
-          >
-            <span>{app}</span>
-          </div>
-        </Command.Item>
-      {/each}
-    </Command.Group>
-    <Command.Separator />
-  </Command.List>
+    <Command.Input placeholder="Type a command or search..." />
+    <Command.List>
+        <Command.Empty>No results found.</Command.Empty>
+        <Command.Group heading="Apps">
+            {#each programs as app}
+                <!-- svelte-ignore a11y-no-static-element-interactions -->
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <div
+                    class="overflow-y-hidden"
+                    on:click={() => {
+                        invoke("runprogram", { path: app });
+                        closeMenu();
+                    }}
+                >
+                    <Command.Item>
+                        <span>{getAppNameFromPath(app)}</span>
+                    </Command.Item>
+                </div>
+            {/each}
+        </Command.Group>
+        <Command.Separator />
+    </Command.List>
 </Command.Dialog>
 <ModeWatcher />
