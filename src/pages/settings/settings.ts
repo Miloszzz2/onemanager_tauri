0f8b537e13c2e4dd5a5b3786e5860a4450ae96f2
Miloszzz2 <97192271+Miloszzz2@users.noreply.github.com>
2024-03-04 20:55:25 +0100

    window for settings managment, improved keyboard shortcut performance for manager opening, improved folder structure, sending data between windows

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,5 @@
-import "./styles.css";
 import Settings from "./Settings.svelte";
-
+import "../../styles.css";
 const app = new Settings({
     target: <HTMLElement>document.getElementById("app"),
 });
